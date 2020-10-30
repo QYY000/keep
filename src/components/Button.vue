93e80e800319803768d02238fc01785dde2d4a03
qyy <1428072793@qq.com>
2020-10-30 20:58:31 +0800

    10/23

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="slotbtn">
+        <slot name="button"></slot>
+    </button>
+</template>
+<style >
+.slotbtn{
+    width: 70px;height: 26px;
+    border-radius: 8px;
+    border:1px solid #707070;
+    line-height: 26px;
+    text-align: center;
+    background-color: #fff;
+    outline: none;
+    padding: 0;margin: 0;
+}
+</style>
